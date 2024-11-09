70b6d0dec436e5ea37387eec4b6ef02fd648ed5f
ZeeTW <zainab.alafoo@hotmail.com>
2024-11-09 13:34:05 +0300

    step 5 done

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board = ['', '', '', '', '', '', '', '', '']
@@ -10,6 +20,33 @@ const squareElements = document.querySelectorAll('.sqr')
 const messageElement = document.querySelector('#message')
 
 /*-------------------------------- Functions --------------------------------*/
+
+const updateBoard = () => {
+  board.forEach((square, i) => {
+    const currentSquare = squareElements[i]
+    if (square === 'x' || square === 'o') {
+      currentSquare.textContent = square
+    } else {
+      currentSquare.textContent = ''
+    }
+  })
+}
+
+const updateMessage = () => {
+  if (winner === false && tie === false) {
+    messageElement.textContent = `${turn}'s turn`
+  } else if (winner === false && tie === true) {
+    messageElement.textContent = "It's a tie!"
+  } else {
+    messageElement.textContent = `${turn} won!`
+  }
+}
+
+const render = () => {
+  updateBoard()
+  updateMessage()
+}
+
 const init = () => {
   render()
 }
